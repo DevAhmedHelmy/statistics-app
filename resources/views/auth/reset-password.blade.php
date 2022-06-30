15cb5dc86ec19ba9a2b91b96d221f82dad5f17a0
ahmed helmy <helmyahmed248@gmail.com>
2022-06-30 09:41:36 +0200

    mail

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -67,7 +67,7 @@
                     </form>
 
                     <p class="text-center mt-2">
-                        <a href="auth-login-basic.html"> <svg xmlns="http://www.w3.org/2000/svg" width="14"
+                        <a href="{{ route('login') }}"> <svg xmlns="http://www.w3.org/2000/svg" width="14"
                                 height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                 stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left">
                                 <polyline points="15 18 9 12 15 6"></polyline>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,80 +9,81 @@
                 </div>
                 <div class="card-body">
 
-                </div>
-                <div class="table-responsive">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>User name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($users as $user)
-                                <tr>
 
-                                    <td>{{ $user->name }}</td>
-                                    <td>
-                                        {{ $user->email }}
-                                    </td>
-                                    <td> {{ $user->phone ?? '---' }}</td>
-                                    <td>
-                                        <div class="dropdown">
-                                            <button type="button"
-                                                class="btn btn-sm dropdown-toggle hide-arrow py-0 waves-effect waves-float waves-light"
-                                                data-bs-toggle="dropdown">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"
-                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                    class="feather feather-more-vertical">
-                                                    <circle cx="12" cy="12" r="1"></circle>
-                                                    <circle cx="12" cy="5" r="1"></circle>
-                                                    <circle cx="12" cy="19" r="1"></circle>
-                                                </svg>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-end">
-                                                <a class="dropdown-item" href="{{ route('users.edit', $user->id) }}">
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>User name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+
+                                        <td>{{ $user->name }}</td>
+                                        <td>
+                                            {{ $user->email }}
+                                        </td>
+                                        <td> {{ $user->phone ?? '---' }}</td>
+                                        <td>
+                                            <div class="dropdown">
+                                                <button type="button"
+                                                    class="btn btn-sm dropdown-toggle hide-arrow py-0 waves-effect waves-float waves-light"
+                                                    data-bs-toggle="dropdown">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"
                                                         viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                                        class="feather feather-edit-2 me-50">
-                                                        <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z">
-                                                        </path>
+                                                        class="feather feather-more-vertical">
+                                                        <circle cx="12" cy="12" r="1"></circle>
+                                                        <circle cx="12" cy="5" r="1"></circle>
+                                                        <circle cx="12" cy="19" r="1"></circle>
                                                     </svg>
-                                                    <span>Edit</span>
-                                                </a>
-                                                <form id="delete-form-{{ $user->id }}"
-                                                    action="{{ route('users.destroy', $user->id) }}" method="post">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button type="button" class="dropdown-item"
-                                                        onclick="confirmDelete('delete-form-{{ $user->id }}')">
+                                                </button>
+                                                <div class="dropdown-menu dropdown-menu-end">
+                                                    <a class="dropdown-item" href="{{ route('users.edit', $user->id) }}">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="14"
                                                             height="14" viewBox="0 0 24 24" fill="none"
                                                             stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                                            stroke-linejoin="round" class="feather feather-trash me-50">
-                                                            <polyline points="3 6 5 6 21 6"></polyline>
+                                                            stroke-linejoin="round" class="feather feather-edit-2 me-50">
                                                             <path
-                                                                d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                                d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z">
                                                             </path>
                                                         </svg>
-                                                        <span>Delete</span>
-                                                    </button>
-                                                </form>
+                                                        <span>Edit</span>
+                                                    </a>
+                                                    <form id="delete-form-{{ $user->id }}"
+                                                        action="{{ route('users.destroy', $user->id) }}" method="post">
+                                                        @csrf
+                                                        @method('delete')
+                                                        <button type="button" class="dropdown-item"
+                                                            onclick="confirmDelete('delete-form-{{ $user->id }}')">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="14"
+                                                                height="14" viewBox="0 0 24 24" fill="none"
+                                                                stroke="currentColor" stroke-width="2"
+                                                                stroke-linecap="round" stroke-linejoin="round"
+                                                                class="feather feather-trash me-50">
+                                                                <polyline points="3 6 5 6 21 6"></polyline>
+                                                                <path
+                                                                    d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                                </path>
+                                                            </svg>
+                                                            <span>Delete</span>
+                                                        </button>
+                                                    </form>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-
-
-                        </tbody>
-                    </table>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    {{ $users->links('pagination::simple-bootstrap-5') }}
                 </div>
-                {{ $users->links('pagination::simple-bootstrap-5') }}
             </div>
         </div>
     </div>
